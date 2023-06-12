3ea0c2ab49a690e52bebfec236a6fb8de07db968
C15HOT <wvxp@mail.ru>
2023-06-12 22:15:32 +0300

    1

--- app/main.py
+++ app/main.py
@@ -5,13 +5,44 @@ import asyncio
 
 settings = get_settings()
 
+#
+# class AsyncIterator:
+#
+#     def __init__(self):
+#         self.counter = 0
+#
+#     def __aiter__(self):
+#         return self
+#
+#     async def __anext__(self):
+#         self.counter += 1
+#         return self.counter
 
-def handler():
+
+async def qwerty():
+    while True:
+        print(list_of_messages)
+        if len(list_of_messages) > 0:
+            message = list_of_messages[0]
+            list_of_messages.remove(message)
+            handler(message)
+        await asyncio.sleep(5)
+
+
+
+
+
+def handler(message):
     import time
     start_time = time.time()
-    result = 99999999**999999
+    print('start')
+    print(message)
+    time.sleep(10)
     print("--- %s seconds ---" % (time.time() - start_time))
+    print('end')
 
+
+list_of_messages = []
 
 async def get_messages():
     rabbit_mq = RabbitMQWrapper()
@@ -22,11 +53,12 @@ async def get_messages():
         async with queue.iterator() as queue_iter:
             async for message in queue_iter:
                 async with message.process():
-                    handler()
-                    print(message.body.decode())
+                    list_of_messages.append(message.body.decode())
 
     await rabbit_mq.shutdown_event_handler()
 
+
 def main():
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(get_messages())
+    loop.run_until_complete(asyncio.gather(get_messages(), qwerty()))
+    # loop.run_until_complete(get_messages())
